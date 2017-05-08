48e71dc8fee431f1d4743f12f0ea638a36c6c170
Chris Ortman <chris-ortman@uiowa.edu>
2017-05-07 21:08:10 -0500

    Adding gol

---
+++ app/src/main/java/com/jamescho/gol/Cell.java
@@ -0,0 +1,43 @@
+package com.jamescho.gol;
+
+import java.util.Random;
+
+public class Cell {
+
+    private static final Random rand = new Random();
+    private boolean isAlive = false;
+    private String id;
+
+    public Cell(String id) {
+        this.id = id;
+    }
+    public Cell(String id, Boolean isAlive) {
+        this.id = id;
+        this.isAlive = isAlive;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void nextState(Cell[] neighbors) {
+
+        if(isAlive && neighbors.length < 2) {
+            isAlive = false;
+        } else if ( isAlive && (neighbors.length == 2 || neighbors.length == 3)) {
+            isAlive = true;
+        } else if (isAlive && neighbors.length > 3) {
+            isAlive = false;
+        } if ( !isAlive && neighbors.length == 3) {
+            isAlive = true;
+        }
+    }
+
+    public void live() {
+        isAlive = true;
+    }
+}
---
+++ app/src/main/java/com/jamescho/gol/GameBoard.java
@@ -0,0 +1,192 @@
+package com.jamescho.gol;
+
+
+import android.graphics.Color;
+import android.view.MotionEvent;
+
+import com.jamescho.framework.util.Painter;
+import com.jamescho.game.state.State;
+import com.jamescho.simpleandroidgdf.GameMainActivity;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class GameBoard extends State {
+
+    private static final int CELL_SIZE = 5;
+    private Cell[][] cells;
+    private int boardSize;
+
+    public GameBoard(int boardSize) {
+        this.boardSize = boardSize;
+    }
+
+    private int getBoardSize() {
+        return cells.length;
+    }
+
+    @Override
+    public void init() {
+        Random r = new Random();
+        cells = new Cell[boardSize][boardSize];
+        for (int i=0; i<boardSize; i++) {
+            for (int j=0; j<boardSize; j++) {
+                String id = i + "x" + j;
+                cells[i][j] = new Cell(id);
+
+                int random = r.nextInt(100);
+                if ( (random % 2 == 0) || random % 3 == 0) {
+                    cells[i][j].live();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void update(float delta) {
+
+        for (int row=0; row<cells.length; row++) {
+            for (int column=0; column<cells[row].length; column++) {
+                Cell[] neighbors = getNeighborsOf(row,column);
+                Cell[] aliveNeighbors = filterAlive(neighbors);
+                cells[column][row].nextState(aliveNeighbors);
+            }
+        }
+    }
+
+    public Cell[] getNeighborsOf2(int row, int column) {
+        ArrayList<Cell> neighbors = new ArrayList<Cell>();
+
+        //if column is 0 then this won't work
+        int columnBefore = column - 1;
+
+        // if column == column count (getBoardSize)
+        int columnAfter = column + 1;
+
+        //if row is 0 then this wont work
+        int rowAbove = row - 1;
+        int rowBelow = row + 1;
+
+        //all the ones above
+        if (rowAbove > -1 && columnBefore > -1) {
+            neighbors.add(cells[rowAbove][columnBefore]);
+        }
+
+        if (rowAbove > -1) {
+            neighbors.add(cells[rowAbove][column]);
+        }
+
+        if (rowAbove > -1 && columnAfter < this.getBoardSize()) {
+            neighbors.add(cells[rowAbove][columnAfter]);
+        }
+
+        if (columnBefore > -1) {
+            neighbors.add(cells[row][columnBefore]);
+        }
+
+        if (columnAfter < this.getBoardSize()) {
+            neighbors.add(cells[row][columnAfter]);
+        }
+
+        if (columnBefore > -1 && rowBelow < this.getBoardSize()) {
+            neighbors.add(cells[rowBelow][columnBefore]);
+        }
+
+        if (rowBelow < this.getBoardSize()) {
+            neighbors.add(cells[rowBelow][column]);
+        }
+
+        if (columnAfter < this.getBoardSize() && rowBelow < this.getBoardSize()) {
+            neighbors.add(cells[rowBelow][columnAfter]);
+        }
+        return neighbors.toArray(new Cell[neighbors.size()]);
+    }
+
+    public Cell[] getNeighborsOf(int row, int column) {
+        ArrayList<Cell> neighbors = new ArrayList();
+        for (int rowIdx = row -1; rowIdx <= row+1; rowIdx++) {
+            for (int colIdx = column - 1; colIdx <= column+1; colIdx++) {
+
+                if (row == rowIdx && column == colIdx) {
+                    //self
+                } else {
+
+                    if( rowIdx > -1 &&
+                            rowIdx < getBoardSize() &&
+                            colIdx > -1 &&
+                            colIdx < getBoardSize()) {
+                        neighbors.add(cells[rowIdx][colIdx]);
+                    }
+                }
+
+            }
+        }
+
+        return neighbors.toArray(new Cell[neighbors.size()]);
+    }
+
+    //This screws up my ordering
+    public Cell[] getNeighborsOf3(int row, int column) {
+        Set<Cell> neighbors = new HashSet<>();
+
+        int columnBefore = Math.min(0, column - 1);
+        int columnAfter = Math.max(getBoardSize()-1, column + 1);
+        int rowAbove = Math.min(0, row - 1);
+        int rowBelow = Math.max(getBoardSize()-1, row + 1);
+
+        //all the ones above
+        neighbors.add(cells[rowAbove][columnBefore]);
+        neighbors.add(cells[rowAbove][column]);
+        neighbors.add(cells[rowAbove][columnAfter]);
+        neighbors.add(cells[row][columnBefore]);
+        neighbors.add(cells[row][columnAfter]);
+        neighbors.add(cells[rowBelow][columnBefore]);
+        neighbors.add(cells[rowBelow][column]);
+        neighbors.add(cells[rowBelow][columnAfter]);
+
+        return neighbors.toArray(new Cell[neighbors.size()]);
+    }
+
+    public Cell[] filterAlive(Cell[] cells) {
+        ArrayList<Cell> aliveCells = new ArrayList<>();
+        for (int i=0; i< cells.length; i++) {
+            if( cells[i].isAlive()) {
+                aliveCells.add(cells[i]);
+            }
+        }
+
+        return aliveCells.toArray(new Cell[aliveCells.size()]);
+    }
+
+    @Override
+    public void render(Painter g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0, GameMainActivity.GAME_WIDTH, GameMainActivity.GAME_HEIGHT);
+
+        for (int i=0; i<cells.length; i++) {
+            for (int j=0; j<cells[i].length; j++) {
+                drawCell(g, i, j);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouch(MotionEvent e, int scaledX, int scaledY) {
+        return false;
+    }
+
+    private void drawCell(Painter g, int i, int j) {
+        Cell c = cells[i][j];
+        if (c.isAlive()) {
+            g.setColor(Color.BLACK);
+        } else {
+            g.setColor(Color.WHITE);
+        }
+
+        g.fillRect(CELL_SIZE * j, CELL_SIZE * i, CELL_SIZE - 1, CELL_SIZE - 1);
+    }
+
+}
